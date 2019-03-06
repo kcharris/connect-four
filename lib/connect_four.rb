1e5f02753f877f9620766c3a1792da1eb9489637
kevin <kcharris77@gmail.com>
2019-03-06 10:18:08 -0600

    add Player class and get input + initialize for Game class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,19 +1,19 @@
 =begin
-Board 7 by 6, players, pieces, game
+Board 7 by 6, players, game
 =end
 class Board
-  attr_accessor :board
+  attr_accessor :show
   #board coordinates must be accessed by 0-5, then number * 2 - 1
   #index of first row in @board is the top of the board
 
   def initialize
-    @board = ["|_|_|_|_|_|_|_|\n",
-              "|_|_|_|_|_|_|_|\n",
-              "|_|_|_|_|_|_|_|\n",
-              "|_|_|_|_|_|_|_|\n",
-              "|_|_|_|_|_|_|_|\n",
-              "|_|_|_|_|_|_|_|\n"]
-    @board << "|1|2|3|4|5|6|7|"
+    @show = ["|_|_|_|_|_|_|_|\n",
+             "|_|_|_|_|_|_|_|\n",
+             "|_|_|_|_|_|_|_|\n",
+             "|_|_|_|_|_|_|_|\n",
+             "|_|_|_|_|_|_|_|\n",
+             "|_|_|_|_|_|_|_|\n"]
+    @show << "|1|2|3|4|5|6|7|\n"
   end
 
   def clear
@@ -23,11 +23,45 @@ class Board
   def enter_move(piece, num)
     num = (num * 2) - 1
     level = 5
-    while level != -1 && @board[level][num] != "_"
+    while level != -1 && @show[level][num] != "_"
       level -= 1
     end
     if level != -1
-      @board[level][num] = piece
+      @show[level][num] = piece
     end
+  end
+end
+
+class Player
+  attr_reader :piece
+  @@number_players = 0
+
+  def initialize
+    @@number_players += 1
+    if @@number_players == 1
+      @piece = "x"
+    else
+      @piece = "o"
+    end
+  end
+end
+
+class Game
+  attr_accessor :board, :player1, :player2
+
+  def initialize
+    @board = Board.new
+    @player1 = Player.new
+    @player2 = Player.new
+  end
+
+  def get_input
+    puts "Attention player, please enter a corresponding number."
+    player_input = gets.chomp
+    until player_input =~ /^[1-7]$/
+      puts "Invalid input, please try again."
+      player_input = gets.chomp
+    end
+    return player_input
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,23 +1,23 @@
 require "./lib/connect_four"
-RSpec.describe Board do
+describe Board do
   describe "#initialize" do
     it "creates a 7 by 6 array of arrays full of 1 space strings" do
       board = Board.new
-      expect(board.board).to eql(["|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n",
-                                  "|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n", "|1|2|3|4|5|6|7|"])
+      expect(board.show).to eql(["|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n",
+                                 "|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n", "|_|_|_|_|_|_|_|\n", "|1|2|3|4|5|6|7|\n"])
     end
   end
   describe "#clear" do
     it "clears the board of pieces" do
       board = Board.new
-      board.board[0][0] = "t"
+      board.show[0][0] = "t"
       board.clear
-      expect(board.board).to eql(Board.new.board)
+      expect(board.show).to eql(Board.new.show)
     end
     it "makes sure there were pieces to clear" do
       board = Board.new
-      board.board[0][0] = "t"
-      expect(board.board).not_to eql(Board.new.board)
+      board.show[0][0] = "t"
+      expect(board.show).not_to eql(Board.new.show)
     end
   end
   describe "#enter_move" do
@@ -25,7 +25,7 @@ RSpec.describe Board do
       board = Board.new
       board.enter_move("o", 4)
       board.enter_move("x", 2)
-      expect(board.board[5][7]).to eql("o")
+      expect(board.show[5][7]).to eql("o")
     end
     it "puts pieces on top of others" do
       board = Board.new
@@ -33,7 +33,34 @@ RSpec.describe Board do
       board.enter_move("a", 2)
       board.enter_move("i", 2)
       #fourth from the top, and 3rd to the right
-      expect(board.board[3][3]).to eql("i")
+      expect(board.show[3][3]).to eql("i")
+    end
+  end
+end
+describe Player do
+  describe "#piece" do
+    it "returns either an x for player 1" do
+      player1 = Player.new
+      expect(player1.piece).to eql("x")
+    end
+    it "returns an o for player 2" do
+      player1 = Player.new
+      player2 = Player.new
+      expect(player2.piece).to eql("o")
+    end
+  end
+end
+describe Game do
+  describe "#get_input" do
+    it "Prompts a player for and accepts a single digit number as an input" do
+      player_input = Game.new.get_input
+      expect(player_input).to eql(4.to_s)
+    end
+  end
+  describe "#initialize" do
+    it "Creates instances of the game variables" do
+      game = Game.new
+      expect(game.board.show).not_to eql(nil)
     end
   end
 end
